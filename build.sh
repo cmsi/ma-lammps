ce1315139e28cfd4da37d64acc66f9b047e6da7f
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2017-10-29 22:38:42 +0900

    Support for Stretch

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ cd $BUILD_DIR
 case "$(dpkg --print-architecture)" in
     amd64)
         dpkg-buildpackage -us -uc
-        mv -f ../${PACKAGE}_$VERSION_BASE*.changes ../${PACKAGE}_$VERSION_BASE*.dsc ../${PACKAGE}_$VERSION_BASE*.debian.tar.* ../${PACKAGE}_$VERSION_BASE*.orig.tar.gz ../${PACKAGE}*_$VERSION_BASE*.deb $TARGET_DIR
+        mv -f ../${PACKAGE}*_$VERSION_BASE*.changes ../${PACKAGE}*_$VERSION_BASE*.buildinfo ../${PACKAGE}*_$VERSION_BASE*.dsc ../${PACKAGE}*_$VERSION_BASE*.debian.tar.* ../${PACKAGE}*_$VERSION_BASE*.orig.tar.gz ../${PACKAGE}*_$VERSION_BASE*.deb $TARGET_DIR
         ;;
     i386)
         cp -fp debian/control-nodoc debian/control
         dpkg-buildpackage -b -us -uc
-        mv -f ../${PACKAGE}_$VERSION_BASE*.changes ../${PACKAGE}*_$VERSION_BASE*.deb $TARGET_DIR
+        mv -f ../${PACKAGE}*_$VERSION_BASE*.changes ../${PACKAGE}*_$VERSION_BASE*.buildinfo ../${PACKAGE}*_$VERSION_BASE*.deb $TARGET_DIR
         ;;
 esac
